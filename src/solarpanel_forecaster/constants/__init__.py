46ae0596303d5ac57a375e7e6810cf100e2cf398
Chris Wickens <chris.wickens49@gmail.com>
2024-09-20 13:22:44 +0100

    fea: added solis private info to environment varaibles + read them in using os. os.environ.get

--- src/solarpanel_forecaster/constants/__init__.py
+++ src/solarpanel_forecaster/constants/__init__.py
@@ -1,6 +1,12 @@
 from pathlib import Path
+import os
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
 CONFIG_SECRET_FILE_PATH = Path("config/secret_keys.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
+
+# Get solis private info
+SOLIS_KEYID = os.environ.get('SOLIS_KEYID', '')
+SOLIS_SECRET_KEY = os.environ.get('SOLIS_SECRET_KEY', '')
+SOLIS_SN = os.environ.get('SOLIS_SN', '')
